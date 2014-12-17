f5a53a4fde8caa4620c878e2805d4cc431a94700
ben.meredith@gmail.com <ben.meredith@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-17 15:48:54 +0000

    change version number
    git-svn-id: https://plugins.svn.wordpress.org/better-click-to-tweet/trunk@1047191 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- better-click-to-tweet.php
+++ better-click-to-tweet.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Better Click To Tweet
 Description: Add click to tweet boxes to your WordPress posts, easily. This is a new, fully renovated version of the late "Click to Tweet" plugin by Todaymade. I overhauled the plugin using the shortcode API, and (perhaps most importantly) removed the "powered by" link.
-Version: 1.0
+Version: 2.0
 Author: Ben Meredith
 Author URI: http://benandjacq.com
 Plugin URI: https://wordpress.org/plugins/better-click-to-tweet/
